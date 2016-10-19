866ba69d777c13d8130e79bb5b9d17320911132d
SusiIrwin <susi.irwin@gmail.com>
2016-10-18 20:32:33 -0600

    Add configuration for VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,11 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock # or :fakeweb
+  config.configure_rspec_metadata!
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end

2ab3e6930b9b4b725807e6abb38c1a009b682d1e
SusiIrwin <susi.irwin@gmail.com>
2016-10-19 10:16:43 -0600

    Create Station Model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,9 @@ class SearchController < ApplicationController
   def index
     @results = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&state=CO&limit=10&api_key=ADDNfDQoSCfrveqS8FFgjEepyBS3enbsvsd3JAzG&format=JSON&radius=6")
     raw_results = JSON.parse(@results.body)
-    # turn hash into object here
-
-    # require 'pry'; binding.pry
+    require 'pry'; binding.pry
+    raw_results.map do |result|
+      Station.new(result)
+    end
   end
 end
---
+++ app/models/station.rb
@@ -0,0 +1,5 @@
+class Station
+  def intialize
+    
+  end
+end
